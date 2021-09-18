247242d1226406cf072d74039aec35e9bc6916ad
mostafa ayman <mostafaaymanpro@gmail.com>
2021-09-18 04:21:50 +0200

    init commit

--- SwiftUI-WeatherApp/ContentView.swift
+++ SwiftUI-WeatherApp/ContentView.swift
@@ -9,8 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+           
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            VStack{
+                Text("Shbien Elkom")
+                    .foregroundColor(.white)
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                
+                VStack(spacing : 12){
+                    Image(systemName: "cloud.sun.fill" )
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 80, height: 50, alignment: .top)
+                    
+                    Text("76°")
+                        .foregroundColor(.white)
+                        .font(.system(size: 22, weight: .medium, design: .default))
+                    
+                }//Image tempreture VSatck
+                
+
+                Spacer()
+            }//VStack
+            VStack(alignment : .leading){
+                Text("Fri")
+                    .font(.system(size: 14, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    
+                Image(systemName:  "cloud.sun.fill")
+                    .renderingMode(.original)
+                    .resizable()
+                    .frame(width: 20, height: 20, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .aspectRatio(contentMode: .fit)
+                    
+            }
+            
+            
+
+        }//ZStack
     }
 }
 
